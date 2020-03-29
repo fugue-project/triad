49bb072496bdea29290381ae28e5f0fa446309d4
Han Wang <hwang@lyft.com>
2020-03-28 18:45:49 -0700

    update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author_email="goodwanghan@gmail.com",
     keywords="util utils utility utilities",
     url="http://github.com/goodwanghan/triad",
-    install_requires=["pandas"],
+    install_requires=["pandas", "ciso8601"],
     extras_require={},
     classifiers=[
         # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
