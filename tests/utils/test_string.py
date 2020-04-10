cbaf2a9c36fa7b4b3b05e85ba301810fb5f12c01
Han Wang <hwang@lyft.com>
2020-04-09 22:12:43 -0700

    add validate var name

---
+++ tests/utils/test_string.py
@@ -0,0 +1,17 @@
+from triad.utils.string import assert_triad_var_name
+from pytest import raises
+
+
+def test_assert_triad_var_name():
+    raises(AssertionError, lambda: assert_triad_var_name(None))
+    raises(AssertionError, lambda: assert_triad_var_name(""))
+    raises(AssertionError, lambda: assert_triad_var_name(" "))
+    raises(AssertionError, lambda: assert_triad_var_name("1"))
+    raises(AssertionError, lambda: assert_triad_var_name(123))
+    raises(AssertionError, lambda: assert_triad_var_name("_"))
+    raises(AssertionError, lambda: assert_triad_var_name("__"))
+    raises(AssertionError, lambda: assert_triad_var_name("a "))
+    raises(AssertionError, lambda: assert_triad_var_name("a a"))
+    assert "a" == assert_triad_var_name("a")
+    assert "_1" == assert_triad_var_name("_1")
+    assert "_a_1" == assert_triad_var_name("_a_1")
