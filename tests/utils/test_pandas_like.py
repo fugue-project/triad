a193859517f8b6f3a28448ea9830b18e158e42f1
Han Wang <hwang@lyft.com>
2020-05-10 13:32:58 -0700

    refactor pandas like utils

--- tests/utils/test_pandas_like.py
+++ tests/utils/test_pandas_like.py
@@ -6,45 +6,43 @@ import numpy as np
 import pandas as pd
 import pyarrow as pa
 from pytest import raises
-from triad.utils.pandas_like import (_ensure_compatible_index,
-                                     as_array_iterable, enforce_type,
-                                     safe_groupby_apply, to_schema)
+from triad.utils.pandas_like import PD_UTILS
 from triad.utils.pyarrow import expression_to_schema
 
 
 def test_to_schema():
     df = pd.DataFrame([[1.0, 2], [2.0, 3]])
-    raises(ValueError, lambda: to_schema(df))
+    raises(ValueError, lambda: PD_UTILS.to_schema(df))
     df = pd.DataFrame([[1.0, 2], [2.0, 3]], columns=["x", "y"])
-    assert list(pa.Schema.from_pandas(df)) == list(to_schema(df))
+    assert list(pa.Schema.from_pandas(df)) == list(PD_UTILS.to_schema(df))
     df = pd.DataFrame([["a", 2], ["b", 3]], columns=["x", "y"])
-    assert list(pa.Schema.from_pandas(df)) == list(to_schema(df))
+    assert list(pa.Schema.from_pandas(df)) == list(PD_UTILS.to_schema(df))
     df = pd.DataFrame([], columns=["x", "y"])
     df = df.astype(dtype={"x": np.int32, "y": np.dtype('object')})
     assert [pa.field("x", pa.int32()), pa.field(
-        "y", pa.string())] == list(to_schema(df))
+        "y", pa.string())] == list(PD_UTILS.to_schema(df))
     df = pd.DataFrame([[1, "x"], [2, "y"]], columns=["x", "y"])
     df = df.astype(dtype={"x": np.int32, "y": np.dtype('object')})
-    assert list(pa.Schema.from_pandas(df)) == list(to_schema(df))
+    assert list(pa.Schema.from_pandas(df)) == list(PD_UTILS.to_schema(df))
     df = pd.DataFrame([[1, "x"], [2, "y"]], columns=["x", "y"])
     df = df.astype(dtype={"x": np.int32, "y": np.dtype(str)})
-    assert list(pa.Schema.from_pandas(df)) == list(to_schema(df))
+    assert list(pa.Schema.from_pandas(df)) == list(PD_UTILS.to_schema(df))
     df = pd.DataFrame([[1, "x"], [2, "y"]], columns=["x", "y"])
     df = df.astype(dtype={"x": np.int32, "y": np.dtype('str')})
-    assert list(pa.Schema.from_pandas(df)) == list(to_schema(df))
+    assert list(pa.Schema.from_pandas(df)) == list(PD_UTILS.to_schema(df))
 
     # test index
     df = pd.DataFrame([[3.0, 2], [2.0, 3]], columns=["x", "y"])
     df = df.sort_values(["x"])
-    assert list(pa.Schema.from_pandas(df)) == list(to_schema(df))
+    assert list(pa.Schema.from_pandas(df)) == list(PD_UTILS.to_schema(df))
     df.index.name = "x"
-    raises(ValueError, lambda: to_schema(df))
+    raises(ValueError, lambda: PD_UTILS.to_schema(df))
     df = df.reset_index(drop=True)
-    assert list(pa.Schema.from_pandas(df)) == list(to_schema(df))
+    assert list(pa.Schema.from_pandas(df)) == list(PD_UTILS.to_schema(df))
     df["p"] = "p"
     df = df.set_index(["p"])
     df.index.name = None
-    raises(ValueError, lambda: to_schema(df))
+    raises(ValueError, lambda: PD_UTILS.to_schema(df))
 
 
 def test_as_array_iterable():
@@ -126,24 +124,24 @@ def test_safe_group_by_apply():
     df = DF([["a", 1], ["a", 2], [None, 3]], "b:str,c:long", True)
 
     def _m1(df):
-        _ensure_compatible_index(df)
+        PD_UTILS.ensure_compatible(df)
         df["ct"] = df.shape[0]
         return df
 
-    res = safe_groupby_apply(df.native, ["b"], _m1)
-    _ensure_compatible_index(res)
+    res = PD_UTILS.safe_groupby_apply(df.native, ["b"], _m1)
+    PD_UTILS.ensure_compatible(res)
     assert 3 == res.shape[0]
     assert 3 == res.shape[1]
     assert [["a", 1, 2], ["a", 2, 2], [None, 3, 1]] == res.values.tolist()
 
-    res = safe_groupby_apply(df.native, [], _m1)
-    _ensure_compatible_index(res)
+    res = PD_UTILS.safe_groupby_apply(df.native, [], _m1)
+    PD_UTILS.ensure_compatible(res)
     assert 3 == res.shape[0]
     assert 3 == res.shape[1]
     assert [["a", 1, 3], ["a", 2, 3], [None, 3, 3]] == res.values.tolist()
 
     df = DF([[1.0, "a"], [1.0, "b"], [None, "c"], [None, "d"]], "b:double,c:str", True)
-    res = safe_groupby_apply(df.native, ["b"], _m1)
+    res = PD_UTILS.safe_groupby_apply(df.native, ["b"], _m1)
     assert [[1.0, "a", 2], [1.0, "b", 2],
             [float('nan'), "c", 2], [float('nan'), "d", 2]].__repr__() == res.values.tolist().__repr__()
 
@@ -152,14 +150,14 @@ class DF(object):  # This is a mock
     def __init__(self, data, schema, enforce=False):
         s = expression_to_schema(schema)
         df = pd.DataFrame(data, columns=s.names)
-        self.native = enforce_type(df, s, enforce)
+        self.native = PD_UTILS.enforce_type(df, s, enforce)
 
     def as_array(self, cols=None, type_safe=False, null_safe=False):
         if cols is None or isinstance(cols, pa.Schema):
-            return list(as_array_iterable(
+            return list(PD_UTILS.as_array_iterable(
                 self.native, schema=cols, type_safe=type_safe, null_safe=null_safe))
         if isinstance(cols, list):
-            os = to_schema(self.native)
+            os = PD_UTILS.to_schema(self.native)
             s = pa.schema([os.field(x) for x in cols])
-            return list(as_array_iterable(
+            return list(PD_UTILS.as_array_iterable(
                 self.native, schema=s, type_safe=type_safe, null_safe=null_safe))
--- triad/utils/pandas_like.py
+++ triad/utils/pandas_like.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Iterable, List, Optional, TypeVar
+from typing import Any, Callable, Iterable, List, Optional, TypeVar, Generic
 
 import numpy as np
 import pandas as pd
@@ -8,154 +8,169 @@ from triad.utils.pyarrow import apply_schema, to_pandas_dtype
 T = TypeVar("T", bound=Any)
 
 
-def as_array_iterable(
-    df: T,
-    schema: Optional[pa.Schema] = None,
-    type_safe: bool = False,
-    null_safe: bool = False,
-) -> Iterable[List[Any]]:
-    """Convert pandas like dataframe to iterable of rows in the format of list.
+class PandasLikeUtils(Generic[T]):
+    def empty(self, df: T) -> bool:
+        return len(df.index) == 0
 
-    :param df: pandas like dataframe
-    :param schema: columns and types for output.
-        Leave it None to return all columns in original type
-    :param type_safe: whether to enforce the types in schema, if not, it will
-        return the original values from the dataframe
-    :param null_safe: whether to ensure returning null for nan or null values in
-        columns with type int, bool and string
-    :return: iterable of rows, each row is a list
+    def as_array_iterable(
+        self,
+        df: T,
+        schema: Optional[pa.Schema] = None,
+        type_safe: bool = False,
+        null_safe: bool = False,
+    ) -> Iterable[List[Any]]:
+        """Convert pandas like dataframe to iterable of rows in the format of list.
 
-    :Notice:
-    * `null_safe` by default is False, for non pandas dataframe, setting it to
-      True may cause errors
-    * If there are nested types in schema, the conversion can be a lot slower
-    """
-    if df.shape[0] == 0:
-        return
-    if schema is None:
-        schema = to_schema(df)
-    else:
-        df = df[schema.names]
-        orig = to_schema(df)
-        if not orig.equals(schema):
-            df = enforce_type(df, schema, null_safe)
-    if not type_safe or all(not pa.types.is_nested(x) for x in schema.types):
-        for arr in df.itertuples(index=False, name=None):
-            yield list(arr)
-    else:
-        # If schema has nested types, the conversion will be much slower
-        for arr in apply_schema(
-            schema,
-            df.itertuples(index=False, name=None),
-            copy=True,
-            deep=True,
-            str_as_json=True,
-        ):
-            yield arr
+        :param df: pandas like dataframe
+        :param schema: columns and types for output.
+            Leave it None to return all columns in original type
+        :param type_safe: whether to enforce the types in schema, if not, it will
+            return the original values from the dataframe
+        :param null_safe: whether to ensure returning null for nan or null values in
+            columns with type int, bool and string
+        :return: iterable of rows, each row is a list
 
-
-def to_schema(df: T) -> pa.Schema:
-    """Extract pandas dataframe schema as pyarrow schema. This is a replacement
-    of pyarrow.Schema.from_pandas, and it can correctly handle string type and
-    empty dataframes
-
-    :param df: pandas dataframe
-    :raises ValueError: if pandas dataframe does not have named schema
-    :return: pyarrow.Schema
-
-    :Notice:
-    The dataframe must be either empty, or with type pd.RangeIndex, pd.Int64Index
-    or pd.UInt64Index and without a name, otherwise, `ValueError` will raise.
-    """
-    _ensure_compatible_index(df)
-    if df.columns.dtype != "object":
-        raise ValueError("Pandas dataframe must have named schema")
-    if isinstance(df, pd.DataFrame) and len(df.index) > 0:
-        return pa.Schema.from_pandas(df)
-    fields: List[pa.Field] = []
-    for i in range(df.shape[1]):
-        tp = df.dtypes[i]
-        if tp == np.dtype("object") or tp == np.dtype(str):
-            t = pa.string()
+        :Notice:
+        * `null_safe` by default is False, for non pandas dataframe, setting it to
+        True may cause errors
+        * If there are nested types in schema, the conversion can be a lot slower
+        """
+        if self.empty(df):
+            return
+        if schema is None:
+            schema = self.to_schema(df)
         else:
-            t = pa.from_numpy_dtype(tp)
-        fields.append(pa.field(df.columns[i], t))
-    return pa.schema(fields)
+            df = df[schema.names]
+            orig = self.to_schema(df)
+            if not orig.equals(schema):
+                df = self.enforce_type(df, schema, null_safe)
+        if not type_safe or all(not pa.types.is_nested(x) for x in schema.types):
+            for arr in df.itertuples(index=False, name=None):
+                yield list(arr)
+        else:
+            # If schema has nested types, the conversion will be much slower
+            for arr in apply_schema(
+                schema,
+                df.itertuples(index=False, name=None),
+                copy=True,
+                deep=True,
+                str_as_json=True,
+            ):
+                yield arr
 
+    def to_schema(self, df: T) -> pa.Schema:
+        """Extract pandas dataframe schema as pyarrow schema. This is a replacement
+        of pyarrow.Schema.from_pandas, and it can correctly handle string type and
+        empty dataframes
 
-def enforce_type(df: T, schema: pa.Schema, null_safe: bool = False) -> T:
-    """Enforce the pandas like dataframe to comply with `schema`.
+        :param df: pandas dataframe
+        :raises ValueError: if pandas dataframe does not have named schema
+        :return: pyarrow.Schema
 
-    :param df: pandas like dataframe
-    :param schema: pyarrow schema
-    :param null_safe: whether to enforce None value for int, string and bool values
-    :return: converted dataframe
+        :Notice:
+        The dataframe must be either empty, or with type pd.RangeIndex, pd.Int64Index
+        or pd.UInt64Index and without a name, otherwise, `ValueError` will raise.
+        """
+        self.ensure_compatible(df)
+        if df.columns.dtype != "object":
+            raise ValueError("Pandas dataframe must have named schema")
+        if isinstance(df, pd.DataFrame) and len(df.index) > 0:
+            return pa.Schema.from_pandas(df)
+        fields: List[pa.Field] = []
+        for i in range(df.shape[1]):
+            tp = df.dtypes[i]
+            if tp == np.dtype("object") or tp == np.dtype(str):
+                t = pa.string()
+            else:
+                t = pa.from_numpy_dtype(tp)
+            fields.append(pa.field(df.columns[i], t))
+        return pa.schema(fields)
 
-    :Notice:
-    When `null_safe` is true, the native column types in the dataframe may change,
-    for example, if a column of `int64` has None values, the output will make sure
-    each value in the column is either None or an integer, however, due to the
-    behavior of pandas like dataframes, the type of the columns may
-    no longer be `int64`
-    """
-    if len(df.index) == 0:
+    def enforce_type(self, df: T, schema: pa.Schema, null_safe: bool = False) -> T:
+        """Enforce the pandas like dataframe to comply with `schema`.
+
+        :param df: pandas like dataframe
+        :param schema: pyarrow schema
+        :param null_safe: whether to enforce None value for int, string and bool values
+        :return: converted dataframe
+
+        :Notice:
+        When `null_safe` is true, the native column types in the dataframe may change,
+        for example, if a column of `int64` has None values, the output will make sure
+        each value in the column is either None or an integer, however, due to the
+        behavior of pandas like dataframes, the type of the columns may
+        no longer be `int64`
+        """
+        if self.empty(df):
+            return df
+        if not null_safe:
+            return df.astype(dtype=to_pandas_dtype(schema))
+        for v in schema:
+            s = df[v.name]
+            if pa.types.is_string(v.type):
+                ns = s[s.isnull()].index.tolist()
+                s = s.astype(str)
+                s.iloc[ns] = None
+            elif pa.types.is_integer(v.type) or pa.types.is_boolean(v.type):
+                ns = s[s.isnull()].index.tolist()
+                s = s.fillna(0).astype(v.type.to_pandas_dtype())
+                s.iloc[ns] = None
+            else:
+                s = s.astype(v.type.to_pandas_dtype())
+            df[v.name] = s
         return df
-    if not null_safe:
-        return df.astype(dtype=to_pandas_dtype(schema))
-    for v in schema:
-        s = df[v.name]
-        if pa.types.is_string(v.type):
-            ns = s[s.isnull()].index.tolist()
-            s = s.astype(str)
-            s.iloc[ns] = None
-        elif pa.types.is_integer(v.type) or pa.types.is_boolean(v.type):
-            ns = s[s.isnull()].index.tolist()
-            s = s.fillna(0).astype(v.type.to_pandas_dtype())
-            s.iloc[ns] = None
-        else:
-            s = s.astype(v.type.to_pandas_dtype())
-        df[v.name] = s
-    return df
+
+    def safe_groupby_apply(
+        self,
+        df: Any,
+        cols: List[str],
+        func: Callable[[T], T],
+        key_col_name="__safe_groupby_key__",
+    ) -> T:
+        """Safe groupby apply operation on pandas like dataframes
+
+        :param df: pandas like dataframe
+        :param cols: columns to group on, can be empty
+        :param func: apply function, df in, df out
+        :param key_col_name: temp key as index for groupu.
+            default "__safe_groupby_key__"
+        :return: output dataframe
+
+        :Notice:
+        The dataframe must be either empty, or with type pd.RangeIndex, pd.Int64Index
+        or pd.UInt64Index and without a name, otherwise, `ValueError` will raise.
+        """
+        self.ensure_compatible(df)
+        if len(cols) == 0:
+            return func(df)
+        keys = df[cols].drop_duplicates()
+        keys[key_col_name] = np.arange(keys.shape[0])
+        df = df.merge(keys, on=cols).set_index([key_col_name])
+
+        def _wrapper(df: T) -> T:
+            return func(df.reset_index(drop=True))
+
+        return df.groupby([key_col_name]).apply(_wrapper).reset_index(drop=True)
+
+    def is_compatile_index(self, df: T) -> bool:
+        if isinstance(df.index, (pd.RangeIndex, pd.Int64Index, pd.UInt64Index)):
+            return True
+        if self.empty(df):
+            return True
+        return False
+
+    def ensure_compatible(self, df: T) -> None:
+        if df.index.name is not None:
+            raise ValueError(f"pandas like datafame index can't have name")
+        if self.is_compatile_index(df):
+            return
+        raise ValueError(
+            f"pandas like datafame must have default index, but got {type(df.index)}"
+        )
 
 
-def safe_groupby_apply(
-    df: Any,
-    cols: List[str],
-    func: Callable[[T], T],
-    key_col_name="__safe_groupby_key__",
-) -> T:
-    """Safe groupby apply operation on pandas like dataframes
-
-    :param df: pandas like dataframe
-    :param cols: columns to group on, can be empty
-    :param func: apply function, df in, df out
-    :param key_col_name: temp key as index for groupu. default "__safe_groupby_key__"
-    :return: output dataframe
-
-    :Notice:
-    The dataframe must be either empty, or with type pd.RangeIndex, pd.Int64Index
-    or pd.UInt64Index and without a name, otherwise, `ValueError` will raise.
-    """
-    _ensure_compatible_index(df)
-    if len(cols) == 0:
-        return func(df)
-    keys = df[cols].drop_duplicates()
-    keys[key_col_name] = np.arange(keys.shape[0])
-    df = df.merge(keys, on=cols).set_index([key_col_name])
-
-    def _wrapper(df: T) -> T:
-        return func(df.reset_index(drop=True))
-
-    return df.groupby([key_col_name]).apply(_wrapper).reset_index(drop=True)
+class PandasUtils(PandasLikeUtils[pd.DataFrame]):
+    pass
 
 
-def _ensure_compatible_index(df: T) -> None:
-    if df.index.name is not None:
-        raise ValueError(f"pandas like datafame index can't have name")
-    if isinstance(df.index, (pd.RangeIndex, pd.Int64Index, pd.UInt64Index)):
-        return
-    if len(df.index) == 0:
-        return
-    raise ValueError(
-        f"pandas like datafame must have default index, but got {type(df.index)}"
-    )
+PD_UTILS = PandasUtils()
