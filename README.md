02039f7817879685b70f55471df0cde82d1ff747
Han Wang <hwang@lyft.com>
2020-05-17 12:20:15 -0700

    Test Coveralls (#27)
    * refactor pandas like utils
    
    * fix groupby apply
    
    * update groupby apply
    
    * test upgrade
    
    * remove dataframes
    
    * remove function utils
    
    * test coverage
    
    * update coveralls
    
    * update
    
    * update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Triad
+
+[![Coverage Status](https://coveralls.io/repos/github/fugue-project/triad/badge.svg)](https://coveralls.io/github/fugue-project/triad)
+
 A collection of python utils functions
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.2.9"
+VERSION = "0.3.0"
 
 with open("README.md") as f:
     LONG_DESCRIPTION = f.read()
@@ -9,14 +9,14 @@ setup(
     name="triad",
     version=VERSION,
     packages=find_packages(),
-    description="A collection of python utils",
+    description="A collection of python utils for Fugue projects",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     license="MIT",
     author="Han Wang",
     author_email="goodwanghan@gmail.com",
     keywords="util utils utility utilities",
-    url="http://github.com/goodwanghan/triad",
+    url="http://github.com/fugue-project/triad",
     install_requires=["pandas", "six", "ciso8601", "pyarrow", "fs"],
     extras_require={},
     classifiers=[
@@ -25,12 +25,10 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
     ],
-    python_requires=">=3",
+    python_requires=">=3.6",
 )
