baf332c5ec269118f65d42516f808749d3ccfdb2
Han Wang <goodwanghan@gmail.com>
2021-05-11 18:53:03 +0000

    improve assert_or_throw

--- triad/utils/assertion.py
+++ triad/utils/assertion.py
@@ -1,24 +1,48 @@
-from typing import Union, Any
+from typing import Any
 from triad.exceptions import NoneArgumentError
 
 
-def assert_or_throw(
-    bool_exp: bool, exception: Union[None, str, Exception] = None
-) -> None:
+def assert_or_throw(bool_exp: bool, exception: Any = None) -> None:
     """Assert on expression and throw custom exception
 
     :param bool_exp: boolean expression to assert on
     :param exception: a custom Exception instance, or any other object that
-        will be stringfied and instantiate an AssertionError
+        will be stringfied and instantiate an AssertionError, or a function
+        that can generate the supported data types
+
+    :Examples:
+
+        .. code-block:: python
+
+            assert_or_throw(True, "assertion error")
+            assert_or_throw(False)  # raise AssertionError
+            assert_or_throw(False, "assertion error")  # raise AssertionError
+            assert_or_throw(False, TypeError("assertion error"))  # raise TypeError
+
+            # Lazy evaluations is useful when constructing the error
+            # itself is expensive or error-prone. With lazy evaluations, happy
+            # path will be fast and error free.
+            def fail():  # a function that is slow and wrong
+                sleep(10)
+                raise TypeError
+
+            assert_or_throw(True, fail())  # (unexpectedly) raise TypeError
+            assert_or_throw(True, fail)  # no exception
+            assert_or_throw(True, lambda: "a" + fail())  # no exception
+            assert_or_throw(False, lambda: "a" + fail())  # raise TypeError
+
     """
     if not bool_exp:
-        if isinstance(exception, Exception):
-            raise exception
-        if isinstance(exception, str):
-            raise AssertionError(exception)
-        if exception is None:
+        _exception: Any = exception
+        if callable(exception):
+            _exception = exception()
+        if _exception is None:
             raise AssertionError()
-        raise AssertionError(str(exception))
+        if isinstance(_exception, Exception):
+            raise _exception
+        if isinstance(_exception, str):
+            raise AssertionError(_exception)
+        raise AssertionError(str(_exception))
 
 
 def assert_arg_not_none(obj: Any, arg_name: str = "", msg: str = "") -> None:
