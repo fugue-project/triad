65b3b801638c587746a5de4f6fe7e3eef62c683c
Han Wang <goodwanghan@gmail.com>
2022-07-08 07:50:55 +0000

    Add SerializableRLock and run_once

--- triad/__init__.py
+++ triad/__init__.py
@@ -3,6 +3,7 @@ from triad_version import __version__
 
 from triad.collections import FileSystem, IndexedOrderedDict, ParamDict, Schema
 from triad.utils import (
+    SerializableRLock,
     assert_arg_not_none,
     assert_or_throw,
     conditional_broadcaster,
@@ -11,5 +12,6 @@ from triad.utils import (
     extension_method,
     make_empty_aware,
     run_at_def,
+    run_once,
     to_uuid,
 )
--- triad/utils/threading.py
+++ triad/utils/threading.py
@@ -1,24 +1,64 @@
+from functools import _make_key, wraps
 from threading import RLock
-from typing import Any, Callable, Dict, Tuple
+from typing import Any, Callable, Dict, Optional, Tuple, Type
 
 
-class RunOnce(object):
-    """Run `func` once, the uniqueness is defined by `key_func`
+class SerializableRLock:
+    """A serialization safe wrapper of :external+python:class:`threading.RLock`"""
+
+    def __init__(self):
+        self._lock = RLock()
+
+    def __enter__(self) -> Any:
+        return self._lock.__enter__()
+
+    def __exit__(
+        self, exception_type: Any, exception_value: Any, exception_traceback: Any
+    ) -> Any:
+        return self._lock.__exit__(exception_type, exception_value, exception_traceback)
+
+    def __getstate__(self) -> Dict[str, Any]:
+        return {}
+
+    def __setstate__(self, data: Dict[str, Any]) -> None:
+        self._lock = RLock()
+
+
+def run_once(
+    func: Optional[Callable] = None,
+    key_func: Optional[Callable] = None,
+    lock_type: Type = RLock,
+) -> Callable:
+    """The decorator to run `func` once, the uniqueness is defined by `key_func`.
+    This implementation is serialization safe and thread safe.
 
     :param func: the function to run only once with this wrapper instance
-    :param key_func: the unique key determined by arguments of `func`
-    :param lock_type: lock class type for thread safe
+    :param key_func: the unique key determined by arguments of `func`, if not set, it
+      will use the same hasing logic as :external+python:func:`functools.lru_cache`
+    :param lock_type: lock class type for thread safe, it doesn't need to be
+      serialization safe
 
     .. admonition:: Examples
 
         .. code-block:: python
 
-            r = RunOnce(max, lambda *args, **kwargs: id(args[0]))
+            @run_once
+            def r(a):
+                return max(a)
+
             a1 = [0, 1]
-            a2 = [0, 1]
-            assert 1 == r(a1) # will trigger max
+            a2 = [0, 2]
+            assert 1 == r(a1) # will trigger r
             assert 1 == r(a1) # will get the result from cache
-            assert 1 == r(a2) # will trigger max again because id(a1)!=id(a2)
+            assert 2 == r(a2) # will trigger r again because of different arguments
+
+            # the following example ignores arguments
+            @run_once(key_func=lambda *args, **kwargs: True)
+            def r2(a):
+                return max(a)
+
+            assert 1 == r(a1) # will trigger r
+            assert 1 == r(a2) # will get the result from cache
 
     .. note::
 
@@ -29,11 +69,44 @@ class RunOnce(object):
           calls will be blocked until the first call returns an result
         * This class is cloudpicklable, but unpickled instance does NOT share the same
           context with the original one
+        * This is not to replace :external+python:func:`functools.lru_cache`,
+          it is not supposed to cache a lot of items
     """
 
-    def __init__(self, func: Callable, key_func: Callable, lock_type: type = RLock):
+    def _run(func: Callable) -> "RunOnce":
+        return wraps(func)(RunOnce(func, key_func=key_func, lock_type=lock_type))
+
+    return _run(func) if func is not None else wraps(func)(_run)  # type: ignore
+
+
+class RunOnce:
+    """Run `func` once, the uniqueness is defined by `key_func`.
+    This implementation is serialization safe and thread safe.
+
+    .. note::
+
+        Please use the decorator :func:`~.run_once` instead of directly
+        using this class
+
+    :param func: the function to run only once with this wrapper instance
+    :param key_func: the unique key determined by arguments of `func`, if not set, it
+        will use the same hasing logic as :external+python:func:`functools.lru_cache`
+    :param lock_type: lock class type for thread safe
+    """
+
+    def __init__(
+        self,
+        func: Callable,
+        key_func: Optional[Callable] = None,
+        lock_type: Type = RLock,
+    ):
         self._func = func
-        self._key_func = key_func
+        if key_func is None:
+            self._key_func: Callable = lambda *args, **kwargs: _make_key(
+                args, kwargs, typed=True
+            )
+        else:
+            self._key_func = key_func
         self._lock_type = lock_type
         self._init_locks()
 
