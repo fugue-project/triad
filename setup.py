634724f29cf6254a2a57eb71eace3b247dc594ba
Han Wang <goodwanghan@gmail.com>
2025-10-30 19:07:00 +0000

    update

--- setup.py
+++ setup.py
@@ -45,12 +45,11 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
